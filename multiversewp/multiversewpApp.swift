493d0ddce5c1a745072f943388c8d8894c35a0b2
Semih Silistre <programc4@gmail.com>
2026-05-17 13:26:09 +0300

    Initial Commit

---
+++ multiversewp/multiversewpApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiversewpApp.swift
+//  multiversewp
+//
+//  Created by Semih Silistre on 17.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct multiversewpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
